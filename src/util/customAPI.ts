c8d23c438d176358430c356b5d465dd6d93e8176
hihijin <117073214+hihijin@users.noreply.github.com>
2023-06-07 00:59:32 +0900

    [Feat] : custom API함수 생성

---
+++ src/util/customAPI.ts
@@ -0,0 +1,35 @@
+import axios, { AxiosError, AxiosInstance, AxiosResponse } from 'axios';
+
+import { getLocalStorage } from './Localstorage';
+
+const baseURL = 'https://www.pre-onboarding-selection-task.shop';
+// const baseURL = 'http://localhost:8000';
+
+// eslint-disable-next-line import/prefer-default-export
+export const Api: AxiosInstance = axios.create({
+	baseURL,
+	timeout: 10000,
+	params: {},
+});
+
+Api.interceptors.response.use(
+	(config: AxiosResponse) => {
+		return config;
+	},
+	(err: AxiosError) => {
+		return Promise.reject(err);
+	},
+);
+
+Api.interceptors.request.use(
+	(config) => {
+		// eslint-disable-next-line no-param-reassign
+		config.headers['Content-Type'] = 'application/json';
+		// eslint-disable-next-line no-param-reassign
+		config.headers.Authorization = `Bearer ${getLocalStorage('accessToken')}`;
+		return config;
+	},
+	(err: AxiosError) => {
+		return Promise.reject(err);
+	},
+);
